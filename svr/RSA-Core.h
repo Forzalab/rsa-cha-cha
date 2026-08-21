7765d3704b9844cf9a30b566a9c98c46cc512281
Forzalab <hbtun9@gmail.com>
2026-08-20 22:03:37 -0700

    feat: basic body for some f7ncs

--- svr/RSA-Core.h
+++ svr/RSA-Core.h
@@ -8,58 +8,84 @@ using Message = cpp_int;
 // namespace std already been applied
 
 // goal: generalizable to js
-class CipherCore {
+class LocksmithBox {
 private:
     // aux value here
     key key_;   // notice - key distinction doesnt really matter. this would be
                 // reused for signing AND encrypting (as well as inverse op)
     cpp_int n_;
+    bool key_is_priv_; // it does matter... ish? this is a consensual flag, camt verify if pub or priv either way at creation.
 
 public:
-    CipherCore(const cpp_int& n, const key& k); // one obj per key hahahahahahhahah
-
+    LocksmithBox(const cpp_int& n, const key& k, bool key_is_priv); // one obj per key, key cannot be reset hahahahahahhahah. how to store keys....?
     cpp_int decimal_from_text(const string& text) const;
     string  text_from_decimal(const cpp_int& big_num) const;
-    void    rotate_key(const cpp_int& P, const cpp_int& Q);
-
-    Message encrypt(const string& message) const;
-    string  decrypt(const Message& message) const;
+ 
+    Message encrypt(const string& message, bool override_keytype_check = false) const;
+    string  decrypt(const Message& encrypted_msg_decimal, bool override_keytype_check = false) const;
     Message sign(const string& message) const;
-    string  verify(const Message& message) const;
+    string  verify(const Message& signed_msg_decimal) const;
 };
 
-inline CipherCore::CipherCore(const cpp_int& n, const key& k)
-    : key_(k), n_(n) {
+inline LocksmithBox::LocksmithBox(const cpp_int& n, const key& k, bool key_is_priv)
+    : key_(k), n_(n), key_is_priv_(key_is_priv) {
 }
 
-inline cpp_int CipherCore::decimal_from_text(const string& text) const {
+inline Message LocksmithBox::decimal_from_text(const string& text) const {
     // text -> one big integer
-    return 0;
+    const Message msg = 0;
+    return msg;
 }
 
-inline string CipherCore::text_from_decimal(const cpp_int& big_num) const {
+inline string LocksmithBox::text_from_decimal(const cpp_int& big_num) const {
     // inverse of decimal_from_text
     return "";
 }
 
-inline void CipherCore::rotate_key(const cpp_int& P, const cpp_int& Q) {
-    // recompute and reseat n_ and key_
+inline Message LocksmithBox::encrypt(const string& message, bool override_keytype_check) const {
+    // prevent dumb user (me)
+    if (key_is_priv_ && !override_keytype_check) { 
+        cout << "[ERROR] priv key attempted for encryption. If you're meant to sign message, use sign() instead" << endl;
+        return cpp_int("67"); // error code instead of crashing - prevent demo embarrasment
+    } else if (override_keytype_check) {
+        cout << "[INFO] Keytype check overriden at encrypt(), ignoring." << endl;
+    }
+    const Message msg_decimal = LocksmithBox::decimal_from_text(message);
+    const key pubkey = this->key_; // assumes stored key is pubkey - func is indiscriminant.
+    const Message encrypted_msg_decimal = powm(msg_decimal, pubkey, n_);
+    return encrypted_msg_decimal;
 }
 
-inline Message CipherCore::encrypt(const string& message) const {
-    return 0;
+inline string LocksmithBox::decrypt(const Message& encrypted_msg_decimal, bool override_keytype_check) const {
+    // prevent dumb user (me)
+    if (!key_is_priv_ && !override_keytype_check) { 
+        cout << "[ERROR] pub key attempted for decryption. If you're meant to verify message, use verify() instead" << endl;
+        return "67"; // error code instead of crashing - prevent demo embarrasment
+    } else if (override_keytype_check) {
+        cout << "[INFO] Keytype check overriden at decrypt(), ignoring." << endl;
+    }
+    const key privkey = this->key_; // assumes stored key is privkey
+    const Message decrypted_msg_decimal = powm(encrypted_msg_decimal, privkey, n_);
+    const string decrypted_message = LocksmithBox::text_from_decimal(decrypted_msg_decimal);
+    return decrypted_message;
 }
 
-inline string CipherCore::decrypt(const Message& message) const {
-    return "";
+inline Message LocksmithBox::sign(const string& message) const {
+    // prevent dumb user (me)
+    if (!key_is_priv_) { 
+        cout << "[ERROR] pub key attempted for signing. If you're meant to encrypt message, use encrypt() instead" << endl;
+        return cpp_int("69"); // error code instead of crashing - prevent demo embarrasment
+    }
+    return this->encrypt(message, true);
 }
 
-inline Message CipherCore::sign(const string& message) const {
-    return 0;
-}
-
-inline string CipherCore::verify(const Message& message) const {
-    return "";
+inline string LocksmithBox::verify(const Message& signed_msg_decimal) const {
+        // prevent dumb user (me)
+    if (key_is_priv_) { 
+        cout << "[ERROR] priv key attempted for sig verify. If you're meant to decrypt message, use decrypt() instead" << endl;
+        return "69"; // error code instead of crashing - prevent demo embarrasment
+    }
+    return this->decrypt(signed_msg_decimal, true);
 }
 
 #endif
