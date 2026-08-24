e71448cd852a4a8f881f4412261d92dde71253c0
Bruc3yX <bruce4xiong@gmail.com>
2026-08-23 17:16:12 -0700

    implemented utilities

--- svr/RSA-Utility.h
+++ svr/RSA-Utility.h
@@ -7,7 +7,9 @@
 #include <boost/math/constants/constants.hpp> //Has pi
 #include <cstdint> // unambigiuous int def
 #include <string> // bcs strings is rad
-
+#include <Bridges.h> // Bridges libs/include 
+#include <boost/random.hpp> // for random num generator 
+#include <GraphAdjList.h> 
 using namespace std; // for convieniece
 
 namespace mp = boost::multiprecision;
@@ -15,7 +17,7 @@ using cpp_int = mp::cpp_int;
 using key = cpp_int;
 using prime_size = uint32_t;
 using URLString = string;
-
+using GraphAdjList = bridges::datastructure::GraphAdjList<string, string, string>;
 // interfaces - avoid .h to reduce complexity for newbies
 // To Bruce: only implement in the brackets below the class braxket, dont fill out anything in Utility class scope!!!!! <3
 // its gonna be a rabbit hole, lol
@@ -31,7 +33,8 @@ public:
     static cpp_int T(const cpp_int& p, const cpp_int& q); // totient
     static key E(const cpp_int& t); /// google for what e should be
     static key D(const key& e, const cpp_int& t);
-    static URLString get_visualization_url(); // MUST return a string - param can be anything u please. if change param, change it both at declaration + definition, or u will get compile err
+    static URLString get_visualization_url(const cpp_int& p, const cpp_int& q, const cpp_int& n, const cpp_int& t, const key& e, const key& d); // MUST return a string - param can be anything u please. if change param, change it both at declaration + definition, or u will get compile err
+	static bridges::Bridges get_bridges();
 };
 
 inline cpp_int Utility::gcd(const cpp_int& a, const cpp_int& b) {
@@ -46,19 +49,19 @@ inline cpp_int Utility::gcd(const cpp_int& a, const cpp_int& b) {
 
 inline cpp_int Utility::get_new_prime(prime_size size) {
     // Implement prime gen here
-	std::boost::random::mt19937 gen(std::random_device{}());
+	boost::random::mt19937 gen(std::random_device{}());
 
-	cpp_int lower = mp::pow(cpp_int(10), digits - 1);
-	cpp_int upper = mp::pow(cpp_int(10), digits) - 1;
+	cpp_int lower = mp::pow(cpp_int(10), size - 1);
+	cpp_int upper = mp::pow(cpp_int(10), size) - 1;
 
 	boost::random::uniform_int_distribution<cpp_int> dist(lower, upper);
-
-	cpp_int candidate;
-	do{
-		candidate = dist(gen);
-		if(candidate % 2 == 0) candidate += 1
-	}while (!mp::miller_rabin_test(candidate,25,gen));
-
+	cpp_int candidate = dist(gen);
+	if(candidate % 2 == 0) candidate += 1;
+	
+	while (!mp::miller_rabin_test(candidate,25,gen)){
+	candidate = dist(gen);
+	if(candidate % 2 == 0) candidate += 1; 
+	}
 	return candidate;
 }
 
@@ -70,18 +73,18 @@ inline cpp_int Utility::N(const cpp_int& p, const cpp_int& q) {
 
 inline cpp_int Utility::T(const cpp_int& p, const cpp_int& q) {
     // T function here
-    T = (p-1) * (q-1);
+   cpp_int  T = (p-1) * (q-1);
 	return T;
 }
 
 inline key Utility::E(const cpp_int& n) {
-    cpp_int E = 65537
+    cpp_int E = 65537;
 	if(gcd(E, n) == 1){
 	return E;
 }    
 E = 3;
 while (gcd(E,n) != 1){
-	e += 2;
+	E += 2;
 }
 	return E;
 }
@@ -107,10 +110,35 @@ if (d<0) d+= n;
 	return d;
 }    
 
-inline URLString Utility::get_visualization_url() {
+inline URLString Utility::get_visualization_url(const cpp_int& p, const cpp_int& q, const cpp_int& n, const cpp_int& t, const key& e, const key& d) {
     // this will need BRIDGES, but just complete the other ones first pls
     // ill try to setup BRIDGES libs ASAP
-    return "";
+string apiKey = "YOUR_API_KEY";
+string userName = "YOUR_USERNAME";
+
+bridges::Bridges myBridges(1, userName, apiKey);
+
+   bridges::datastructure::GraphAdjList<string, string, string> graph;
+
+    graph.addVertex("p", p.str());
+    graph.addVertex("q", q.str());
+    graph.addVertex("N", n.str());
+    graph.addVertex("totient", t.str());
+    graph.addVertex("e", e.str());
+    graph.addVertex("d", d.str());
+
+    graph.addEdge("p", "N", "p*q");
+    graph.addEdge("q", "N", "p*q");
+    graph.addEdge("p", "totient", "(p-1)(q-1)");
+    graph.addEdge("q", "totient", "(p-1)(q-1)");
+    graph.addEdge("totient", "e", "coprime to totient");
+    graph.addEdge("e", "d", "modular inverse mod totient");
+
+myBridges.setDataStructure(graph);
+myBridges.visualize();
+
+string url = myBridges.getVisualizeURL();
+	return url;
 }
 
 #endif
